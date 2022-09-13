4f480374da85c85c689643b0042c03ed5ab60e78
viyan-najmaldin <najmadinviyan@gmail.com>
2022-09-14 00:34:11 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+  { year: "2018", result: "N/A" },
+  { year: "2017", result: "N/A" },
+  { year: "2016", result: "N/A" },
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "1963", result: "N/A" },
+  { year: "1961", result: "N/A" },
+];
+function superbowlWin(arr) {
+  let win = arr.find((record) => record.result === "W");
+  return win ? win.year : undefined;
+}
